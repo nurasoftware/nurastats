b520bda759e87e6c3cb4b9d0b8c78b9c7aff2cd2
Gabriel Chimilevschi <gabriel@clevada.com>
2025-06-15 18:58:30 +0300

    Assets upload

---
+++ public/assets/js/main.js
@@ -0,0 +1,38 @@
+jQuery(function ($) {
+    'use strict';
+	
+	// Header Sticky
+	$(window).on('scroll',function() {
+		if ($(this).scrollTop() > 120){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		}
+	});
+	
+	
+	// Others Option For Responsive JS
+	$(".others-option-for-responsive .dot-menu").on("click", function(){
+		$(".others-option-for-responsive .container .container").toggleClass("active");
+	});
+
+	
+	// Go to Top
+	$(function(){
+		// Scroll Event
+		$(window).on('scroll', function(){
+			var scrolled = $(window).scrollTop();
+			if (scrolled > 600) $('.go-top').addClass('active');
+			if (scrolled < 600) $('.go-top').removeClass('active');
+		});  
+		// Click Event
+		$('.go-top').on('click', function() {
+			$("html, body").animate({ scrollTop: "0" },  500);
+		});
+	});
+	
+}(jQuery));
+
+
+
